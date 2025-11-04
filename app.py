e74b8aee50650f3cf3f5cb885036c5926d760a31
jcjaylani <joan.jaylani@gmail.com>
2025-11-04 10:02:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 # Set page configuration for the Streamlit app
 st.set_page_config(page_title="Modular Data Explorer", layout="wide")
 
@@ -124,5 +123,4 @@ with tabs[6]:
         st.info("Please upload a dataset to generate AI insights.")
     else:
         ai_review.show_ai_review(st.session_state['df'], st.session_state.get('ml_results'))
-
 
